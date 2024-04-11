92d01f873ae21a562e52690a1900c506ee42245b
mssp1 <msspiccolo1717@gmail.com>
2024-04-11 00:55:55 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'dart:convert' show json;
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
+
+void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
 
@@ -22,10 +28,48 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage>{
+
+  List<dynamic> products = [];
+
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchPosts();
   }
 
+Future<void> fetchPosts() async{
+
+final response = await http.get(
+  Uri.parse('https://app-uniara-eb91fc9ec7bf.herokuapp.com/list')
+  
+);
+ if (response.statusCode == 200) 
+ {
+    setState((){
+    String data =response.body;
+    products = json.decode(data);
+
+  });
+
+ }else {
+  throw Exception('Não consegui chamar o serviço que o Prof.Andre criou!');
+ }
+}}
+ @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Lista de produtos'),
+    ),
+    body: ListView.separated(
+      separatorBuilder: (BuildContext context, int index) => Divider(),  
+      itemCount: product.length,
+      itemBuilder: (BuildContext context, int index){
+        return ListTile(
+          title: Text(products[index]['description']), // Corrigir aqui
+        );
+      }
+    )
+  );
 }
